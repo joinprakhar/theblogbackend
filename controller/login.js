30772bd91b2fa5031e1b98115030ddb7e49c5238
joinprakhar <pandeylive@gmail.com>
2023-07-05 14:42:02 +0530

    kb

--- controller/login.js
+++ controller/login.js
@@ -8,25 +8,26 @@ const secret = "76b7u76u7u6bfxnghnchg7yjyujjjy";
 const login = async (req, res) => {
     const { email, password } = req.body;
     const userDoc = await User.findOne({ email })
-    
-    const passOk = bcrypt.compareSync(password, userDoc.password);
-    if (passOk) {
-        const Name = userDoc.firstName + " " + userDoc.lastName
-        //logedIn
-        jwt.sign({ email, id: userDoc._id, Name}, secret, {}, (err, token) => {
-            if (err) throw err;
-            res.cookie('token', token).json({
-                id: userDoc._id,
-                email,
-                Name
-            });
-        });
-        console.log("done")
-        //res.json()
-    } else {
-        res.status(400).json('wrong Credentials')
-        console.log("notdone")
-    }
+    console.log(password);
+    console.log(userDoc.password);
+    // const passOk = bcrypt.compareSync(password, userDoc.password);
+    // if (passOk) {
+    //     const Name = userDoc.firstName + " " + userDoc.lastName
+    //     //logedIn
+    //     jwt.sign({ email, id: userDoc._id, Name}, secret, {}, (err, token) => {
+    //         if (err) throw err;
+    //         res.cookie('token', token).json({
+    //             id: userDoc._id,
+    //             email,
+    //             Name
+    //         });
+    //     });
+    //     console.log("done")
+    //     //res.json()
+    // } else {
+    //     res.status(400).json('wrong Credentials')
+    //     console.log("notdone")
+    // }
 }
 
 module.exports =login
