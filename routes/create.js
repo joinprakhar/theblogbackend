164ee63b16e5349111850f826afd94fab1911f55
joinprakhar <pandeylive@gmail.com>
2023-07-09 21:21:51 +0530

    hh

--- routes/create.js
+++ routes/create.js
@@ -24,7 +24,7 @@ const create = async (req, res) => {
             image,
             category,
             //cover: newPath,
-            filepath: path,
+            //filepath: path,
             author: userDoc._id
         });
         res.json(postDoc);
