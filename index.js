a31b48e1e90d91b2fe82141b72c97c3db6275851
joinprakhar <pandeylive@gmail.com>
2023-07-10 16:58:52 +0530

    aa

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }))
 app.use(express.json())
-app.use(cookieParser({ domain: 'https://blogbackend-e8fr.onrender.com' }));
+app.use(cookieParser());
 app.use('', express.static(__dirname + 'api/uploads'));
 // app.use(express.static(path.join(__dirname , './client/build'))); || "https://thepost.onrender.com""https://theblogpost.netlify.app"
 
