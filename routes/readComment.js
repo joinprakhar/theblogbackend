7b2eb28b330907f7d7a2256a725edaf17b6742dc
joinprakhar <pandeylive@gmail.com>
2023-07-11 14:45:54 +0530

    zaa

--- routes/readComment.js
+++ routes/readComment.js
@@ -5,8 +5,8 @@ const Comment = require('./commentSchema');
 
 const postId = async (req, res) => {
     const { id } = req.params;
-    // const result = await Comment.find({ blog : { $in: id } })
-    // res.json(result);
+    const result = await Comment.find({ blog : { $in: id } })
+    res.json(result);
 }
 
 module.exports = postId;
