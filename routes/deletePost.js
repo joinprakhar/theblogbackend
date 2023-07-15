cf5f38493eed2821a34f98be511c5997a61acba8
joinprakhar <pandeylive@gmail.com>
2023-07-15 16:23:11 +0530

    updated delete

--- routes/deletePost.js
+++ routes/deletePost.js
@@ -8,14 +8,36 @@ const secret = "76b7u76u7u6bfxnghnchg7yjyujjjy";
 app.use(cookieParser());
 
 const deletePost = async (req, res) => {
-        const { id , userId } = req.body;
+    try {
+        const { id, userId } = req.body;
         const postId = await Post.findById(id);
-        const isAuthor = JSON.stringify(postId.author) == JSON.stringify(userId);
+        const isAuthor = JSON.stringify(postId.author) === JSON.stringify(userId);
+
         if (!isAuthor) {
-            return res.status(400).json('you are not the author');
+            return res.status(400).json('You are not the author');
         }
-        await Post.findByIdAndRemove(postId._id)
-        res.json(postId);
-}
+
+        await Post.findByIdAndRemove(postId._id);
+
+        // Return success response
+        res.status(200).json('Post deleted successfully');
+    } catch (error) {
+        // Return error response
+        res.status(500).json('An error occurred while deleting the post');
+    }
+};
+
+
+// const deletePost = async (req, res) => {
+    
+//         const { id , userId } = req.body;
+//         const postId = await Post.findById(id);
+//         const isAuthor = JSON.stringify(postId.author) == JSON.stringify(userId);
+//         if (!isAuthor) {
+//             return res.status(400).json('you are not the author');
+//         }
+//         await Post.findByIdAndRemove(postId._id)
+//         res.json(postId);
+// }
 
 module.exports = deletePost
